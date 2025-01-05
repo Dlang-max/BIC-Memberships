2820459ffc731427dd92847ea7a1faf22ba1627b
Dlang-max <langd052405@gmail.com>
2025-01-05 17:15:05 -0500

    Adding try excepty blocks to code. Also created a prod docker compose file. Push to an AWS EC2 instance

--- script.py
+++ script.py
@@ -10,12 +10,16 @@ USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 
 # Establish database connection
-database_connection = mysql.connector.connect(
-    host=HOST,
-    database=DATABASE_NAME,
-    user=USER,
-    password=PASSWORD
-)
+try:
+    database_connection = mysql.connector.connect(
+        host=HOST,
+        database=DATABASE_NAME,
+        user=USER,
+        password=PASSWORD
+    )
+except mysql.connector.Error as err:
+    print("Error Connecting to MySQL DB")
+    exit()
 
 # Establish connection to Google Sheets
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive']
@@ -23,10 +27,6 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('./key.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open('Buffalo Irish Center Active Members')
 sheet = sheet.worksheet('Active Members')
-
-# The cursor allows use to process information from the mySQL database one
-# row at a time. 
-cursor = database_connection.cursor()
 
 # SQL query to get the first and last name of users with active subscriptions
 query = """
@@ -49,17 +49,26 @@ ORDER BY
     meta_last.meta_value ASC;
 """
 
-# Execute the query
-cursor.execute(query)
+try: 
+    # The cursor allows use to process information from the mySQL database one
+    # row at a time. 
+    cursor = database_connection.cursor()
+    cursor.execute(query)
 
-# Update the Google Sheet:
-# Using pandas DataFrame instead of repeatedly querying and updating
-# Google Sheet. Want to avoid getting rate limited.
-df = pd.DataFrame(columns=["First Name", "Last Name", "Membership Status"])
-for idx, (user_id, first_name, last_name, status) in enumerate(cursor):
-    df.loc[len(df)] = [first_name, last_name, status]
-    print(f"{first_name} {last_name} -> {status}")
-sheet.update([df.columns.values.tolist()] + df.values.tolist())
+    data = []
+    for (_, first_name, last_name, status) in cursor:
+        print(f"{first_name} {last_name} -> {status}")
+        data.append([first_name, last_name, status])
 
-cursor.close()
-database_connection.close()
+    # Update the Google Sheet:
+    # Using pandas DataFrame instead of repeatedly querying and updating
+    # Google Sheet. Want to avoid getting rate limited.
+    df = pd.DataFrame(data=data, columns=["First Name", "Last Name", "Membership Status"])
+    sheet.clear()
+    sheet.update([df.columns.values.tolist()] + df.values.tolist())
+except Exception as e:
+    print("Error updating Google Sheet")
+    exit()
+finally:
+    cursor.close()
+    database_connection.close()
